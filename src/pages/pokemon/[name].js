73687a363f36eaff3c9a1ae8cf742ba75419babf
Ty <ty.streets@irvmat.com>
2023-11-13 19:50:23 -0500

    Pulling in pokemon info by name

--- src/pages/pokemon/[name].js
+++ src/pages/pokemon/[name].js
@@ -1,14 +1,41 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { Loader, Image } from 'semantic-ui-react';
 
 export default function PokemonName() {
-    const router = useRouter();
+  const [pokemonInfo, setPokemonInfo] = React.useState({ loading: true });
+  const router = useRouter();
 
-    // console.log(router.query);
+  React.useEffect(function () {
+    if (pokemonInfo.name !== router.query.name && router.query.name) {
+      console.log('Load in pokemon info', router.query.name);
+      fetch(`https://pokeapi.co/api/v2/pokemon/${router.query.name}`)
+        .then((r) => r.json())
+        .then(function (r) {
+          setPokemonInfo(r);
+        })
+        .catch((e) => setPokemonInfo({ loading: false, name: router.query.name }));
+    }
+  });
 
-    return (
-        <>
-            <h1>Pokemon Name: {router.query.name}</h1>
-        </>
-    )
+  // console.log(router.query);
+
+  return (
+    <>
+      <h1>Pokemon Name: {router.query.name}</h1>
+      <Loader active={pokemonInfo.loading || pokemonInfo.name !== router.query.name} />
+      {
+        // if this statement
+        pokemonInfo.id ? (
+          // then do this; true
+          <>
+            <Image src={pokemonInfo.sprites.front_default}></Image>
+          </>
+        ) : (
+          // else do this; false
+          <>{isNaN(pokemonInfo.id) ? <h2>Searching for Pokemon</h2> : <h2>Pokemon Not Found</h2>}</>
+        )
+      }
+    </>
+  );
 }
